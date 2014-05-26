84202f3b2abb803c05f9d53c4a7a3034bb6056b9
Victor Alenkov <Victor@Alenkov.info>
2014-05-26 04:34:25 +0400

    Перенесли тесты в общий абстрактный класс

---
+++ src/test/java/ru/itbasis/demo/testng/abstract_tests/AbstractTests.java
@@ -0,0 +1,75 @@
+package ru.itbasis.demo.testng.abstract_tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import ru.itbasis.demo.testng.abstract_tests.model.Department;
+import ru.itbasis.demo.testng.abstract_tests.model.Employee;
+import ru.itbasis.demo.testng.abstract_tests.model.Organization;
+
+import java.io.File;
+import java.util.List;
+
+abstract public class AbstractTests {
+	protected Organization organization;
+
+	abstract protected String getOrgName();
+
+	@DataProvider
+	public Object[][] dataDepartmentCount() {
+		return new Object[][]{};
+	}
+
+	@DataProvider
+	public Object[][] dataEmployeeCount() {
+		return new Object[][]{};
+	}
+
+	@DataProvider
+	public Object[][] dataEmployeeLastName() {
+		return new Object[][]{};
+	}
+
+	@BeforeMethod
+	public void setUp() throws Exception {
+		File fRoot = new File(".");
+		File orgDir = new File(fRoot, "src/test/resources/" + getOrgName());
+		Assert.assertTrue(orgDir.exists());
+		Assert.assertTrue(orgDir.isDirectory());
+
+		organization = OrganizationLoader.loader(orgDir);
+		Assert.assertNotNull(organization);
+	}
+
+	@Test(dataProvider = "dataDepartmentCount")
+	public void testDepartmentCount(int count) throws Exception {
+		Assert.assertEquals(organization.getDepartments().size(), count);
+	}
+
+	@Test(dependsOnMethods = {"testDepartmentCount"}, dataProvider = "dataEmployeeCount")
+	public void testEmployeesCount(String depName, int countEmployee) throws Exception {
+		final List<Department> departments = organization.getDepartments();
+		int i = 0;
+		Department department;
+		do {
+			department = departments.get(i++);
+		} while (!department.getName().equals(depName));
+		Assert.assertEquals(department.getName(), depName);
+		Assert.assertEquals(department.getEmployees().size(), countEmployee);
+	}
+
+	@Test(dependsOnMethods = {"testDepartmentCount"}, dataProvider = "dataEmployeeLastName")
+	public void testEmployeeLastName(String depName, int employeeIndex, String lastName) throws Exception {
+		final List<Department> departments = organization.getDepartments();
+		int i = 0;
+		Department department;
+		do {
+			department = departments.get(i++);
+		} while (!department.getName().equals(depName));
+		Assert.assertEquals(department.getName(), depName);
+		Employee employee = department.getEmployees().get(employeeIndex);
+		Assert.assertEquals(employee.getLastName(), lastName);
+	}
+
+}
--- src/test/java/ru/itbasis/demo/testng/abstract_tests/Organization0Test.java
+++ src/test/java/ru/itbasis/demo/testng/abstract_tests/Organization0Test.java
@@ -1,67 +1,29 @@
 package ru.itbasis.demo.testng.abstract_tests;
 
-import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-import ru.itbasis.demo.testng.abstract_tests.model.Department;
-import ru.itbasis.demo.testng.abstract_tests.model.Employee;
-import ru.itbasis.demo.testng.abstract_tests.model.Organization;
 
-import java.io.File;
-import java.util.List;
+public class Organization0Test extends AbstractTests {
 
-public class Organization0Test {
-	private Organization organization;
+	@Override
+	protected String getOrgName() {
+		return "org0";
+	}
 
 	@DataProvider
+	@Override
+	public Object[][] dataDepartmentCount() {
+		return new Object[][]{{2}};
+	}
+
+	@DataProvider
+	@Override
 	public Object[][] dataEmployeeCount() {
 		return new Object[][]{{"dep1", 2}};
 	}
 
 	@DataProvider
+	@Override
 	public Object[][] dataEmployeeLastName() {
 		return new Object[][]{{"dep0", 0, "empLastName0_0"}};
-	}
-
-	@BeforeMethod
-	public void setUp() throws Exception {
-		File fRoot = new File(".");
-		File orgDir = new File(fRoot, "src/test/resources/org0");
-		Assert.assertTrue(orgDir.exists());
-		Assert.assertTrue(orgDir.isDirectory());
-
-		organization = OrganizationLoader.loader(orgDir);
-		Assert.assertNotNull(organization);
-	}
-
-	@Test
-	public void testDepartmentCount() throws Exception {
-		Assert.assertEquals(organization.getDepartments().size(), 2);
-	}
-
-	@Test(dependsOnMethods = {"testDepartmentCount"}, dataProvider = "dataEmployeeCount")
-	public void testEmployeesCount(String depName, int countEmployee) throws Exception {
-		final List<Department> departments = organization.getDepartments();
-		int i = 0;
-		Department department;
-		do {
-			department = departments.get(i++);
-		} while (!department.getName().equals(depName));
-		Assert.assertEquals(department.getName(), depName);
-		Assert.assertEquals(department.getEmployees().size(), countEmployee);
-	}
-
-	@Test(dependsOnMethods = {"testDepartmentCount"}, dataProvider = "dataEmployeeLastName")
-	public void testEmployeeLastName(String depName, int employeeIndex, String lastName) throws Exception {
-		final List<Department> departments = organization.getDepartments();
-		int i = 0;
-		Department department;
-		do {
-			department = departments.get(i++);
-		} while (!department.getName().equals(depName));
-		Assert.assertEquals(department.getName(), depName);
-		Employee employee = department.getEmployees().get(employeeIndex);
-		Assert.assertEquals(employee.getLastName(), lastName);
 	}
 }
